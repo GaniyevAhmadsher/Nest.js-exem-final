32af0c2792531cc66c2417290142a2336c435f68
G'aniyev Ahmadsher <ganievahmadsher6205@gmail.com>
2025-05-21 19:57:44 +0500

    Admin

--- src/modules/admins/admins.controller.ts
+++ src/modules/admins/admins.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AdminsService } from './admins.service';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorator/role.decorator';
 
 @Controller('admins')
+@UseGuards(AuthGuard, RoleGuard)
+@Roles('ADMIN')
 export class AdminsController {
   constructor(private readonly adminsService: AdminsService) {}
 }
